05b44910ac08165734308c321927ceea4cd5888a
Mickey Pashov <pashov.m@gmail.com>
2015-03-23 21:37:31 +0000

    Comments on the Notes model

--- noted/notes/models.py
+++ noted/notes/models.py
@@ -2,11 +2,22 @@ from django.db import models
 
 # Create your models here.
 class Note(models.Model):
+    """
+    A Note represents a single unit of information
+    A note is made up of a single titel and body section (the content)
+    Timestamps are automatically generated with a creation of a new note
+
+    Each note can have many Tags
+    Each note exists in only one Folder at a time.
+
+    It is not possible for a single note to exist in two different folders.
+    If the note is copied, a new note is created. If the new note is edited, the original will not be changed.
+    """
     title = models.CharField(max_length=200)
-    #author = models.ForeignKey(User)
+    # author = models.ForeignKey(User)
+    # folder = models.ForeignKey(Folder)
     body = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-
     tags = models.ManyToManyField('Tag', related_name='notes', blank=True)
 
     def __unicode__(self):
@@ -14,14 +25,19 @@ class Note(models.Model):
 
 
 class Tag(models.Model):
+    """
+
+    """
     label = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50)
+
     def __unicode__(self):
         return self.label
 
+
 class Folder(models.Model):
     title = models.CharField(max_length=50)
-    #owner = models.ForeignKey(User)
+    # owner = models.ForeignKey(User)
     note = models.ManyToManyField(Note)
 
     def __unicode__(self):
