4ff85051f916aee7665313b0fb41ad5de3277191
meras <allmost@gmail.com>
2015-03-23 00:46:51 +0000

    add/edit notes from the main page

--- noted/notes/views.py
+++ noted/notes/views.py
@@ -6,42 +6,19 @@ from models import Note, Tag
 from forms import NoteForm, TagForm
 from django.utils.text import slugify
 from django.contrib.auth.decorators import user_passes_test
-
 from django.http import JsonResponse
 
 
 def superuser_only(user):
     return (user.is_authenticated() and user.is_superuser)
 
+
 @user_passes_test(superuser_only, login_url="/")
 def index_view(request):
     notes = Note.objects.all().order_by('-timestamp')
     tags = Tag.objects.all()
-    return render(request, 'notes/index.html', {'notes':notes, 'tags':tags})
+    return render(request, 'notes/index.html', {'notes': notes, 'tags': tags})
 
-@user_passes_test(superuser_only, login_url="/")
-def add_note(request):
-    id = request.GET.get('id', None)
-    if id is not None:
-        note = get_object_or_404(Note, id=id)
-    else:
-        note = None
-
-    if request.method =='POST':
-        if request.POST.get('control') == 'delete':
-            note.delete()
-            messages.add_message(request, messages.INFO, "Note deleted")
-            return HttpResponseRedirect(reverse('notes:index'))
-
-        form = NoteForm(request.POST, instance=note)
-        if form.is_valid():
-            form.save()
-            messages.add_message(request, messages.INFO, "Note added")
-            return HttpResponseRedirect(reverse('notes:index'))
-    else:
-        form = NoteForm(instance=note)
-
-    return render(request, 'notes/addnote.html', {'form':form, 'note':note})
 
 @user_passes_test(superuser_only, login_url="/")
 def add_tag(request):
@@ -50,7 +27,7 @@ def add_tag(request):
         tag = get_object_or_404(Tag, id=id)
     else:
         tag = None
-    
+
     if request.method == 'POST':
         if request.POST.get('control') == 'delete':
             tag.delete()
@@ -61,12 +38,11 @@ def add_tag(request):
             t = form.save(commit=False)
             t.slug = slugify(t.label)
             t.save()
-            messages.add_message(request,messages.INFO, 'Tag deleted')
+            messages.add_message(request, messages.INFO, 'Tag deleted')
             return HttpResponseRedirect(reverse('notes:index'))
     else:
         form = TagForm(instance=tag)
-    return render(request, 'notes/addtag.html', {'form':form, 'tag':tag})
-
+    return render(request, 'notes/addtag.html', {'form': form, 'tag': tag})
 
 
 @user_passes_test(superuser_only, login_url="/")
@@ -76,8 +52,36 @@ def note_content(request):
         note_id = request.GET['note_id']
 
     note = None
-    print Note.objects.get(pk=note_id)
     if note_id:
         note = Note.objects.get(pk=note_id)
 
     return JsonResponse({'title': note.label, 'body': note.body})
+
+
+@user_passes_test(superuser_only, login_url="/")
+def add_note(request):
+    id = request.GET.get('id', None)
+    if id is not None:
+        note = get_object_or_404(Note, id=id)
+    else:
+        note = None
+
+    if request.method == 'POST':
+        if request.POST.get('control') == 'delete':
+            note.delete()
+            messages.add_message(request, messages.INFO, "Note deleted")
+            return HttpResponseRedirect(reverse('notes:index'))
+
+        id = request.POST.get('id')
+        if id is not None:
+            note = get_object_or_404(Note, id=id)
+
+        form = NoteForm(request.POST, instance=note)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.INFO, "Note added")
+            return HttpResponseRedirect(reverse('notes:index'))
+    else:
+        form = NoteForm(instance=note)
+
+    return render(request, 'notes/addnote.html', {'form': form, 'note': note})
--- noted/static/noted/js/ajax.js
+++ noted/static/noted/js/ajax.js
@@ -13,11 +13,48 @@ $(document).ready(function() {
     //    });
     //});
 
+    //          Demonstating how to delete a post
+    //
+    //$.post("/notes/addnote/?id=9", {control: 'delete',
+    //                       csrfmiddlewaretoken:$.cookie('csrftoken')
+    //                      });
+
+    // this saves a note as a new instance
+    $('.btn-success').click(function(){
+        var csrftoken = $.cookie('csrftoken');
+        var note_title = $('.note-info > section').html();
+        var note_content = $('.editor').html();
+        var data = {
+            label: note_title,
+            body: note_content,
+            csrfmiddlewaretoken:$.cookie('csrftoken')};
+
+        $.post("/notes/addnote/", data);
+    });
+
+    //edit an existing note
+    $('.btn-info').click(function(){
+        var note_id = $('.note-info').attr('id');
+        var csrftoken = $.cookie('csrftoken');
+        var note_title = $('.note-info > section').html();
+        var note_content = $('.editor').html();
+        var data = {
+            id: note_id,
+            label: note_title,
+            body: note_content,
+            csrfmiddlewaretoken:$.cookie('csrftoken')};
+        console.log(note_id);
+
+        $.post("/notes/addnote/", data);
+    });
+
+    // retrieve a note once clicked on a list
     $('.note').click(function(){
         var note_id = $(this).attr("data-noteid");
 
         $.getJSON('/notes/note/', {note_id: note_id}, function(data){
-            $('.note-info').html(data.title);
+            $('.note-info').attr('id', note_id);
+            $('.note-info > section').html(data.title);
             $('.editor').html(data.body);
         });
     });
