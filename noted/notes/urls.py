0b55a969f5375eb754e2aba8d05110e50b6862f2
Mickey Pashov <pashov.m@gmail.com>
2015-03-24 19:17:14 +0000

    Removing some redundant urls

--- noted/notes/urls.py
+++ noted/notes/urls.py
@@ -3,12 +3,12 @@ from django.contrib import admin
 from views import *
 
 urlpatterns = patterns('',
-	url(r'^$', index_view, name='index'),
-	url(r'^addnote/', add_note, name='addnote'),
-	url(r'^editnote/', edit_note, name='editnote'),
-	url(r'^deletenote/', delete_note, name='deletenote'),
-	url(r'^addtag/', add_tag, name='addtag'),
-	url(r'^note/$', note_content, name='note'),
-    url(r'^folder/(?P<folder_title_slug>[\w\-]+)/$', folder, name='folder'),
-    url(r'^addfolder/$', add_folder, name='addfolder')
-	)
+                       url(r'^$', index_view, name='index'),
+                       url(r'^addnote/', add_note, name='addnote'),
+                       url(r'^editnote/', edit_note, name='editnote'),
+                       url(r'^deletenote/', delete_note, name='deletenote'),
+                       url(r'^addtag/', add_tag, name='addtag'),
+                       url(r'^note/$', note_content, name='note'),
+                       url(r'^folder/(?P<folder_title_slug>[\w\-]+)/$', folder, name='folder'),
+                       url(r'^addfolder/$', add_folder, name='addfolder')
+)
