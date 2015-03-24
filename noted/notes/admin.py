9f26e05c3cfdd34c032f9de11a16be9a73f5582b
Mickey Pashov <pashov.m@gmail.com>
2015-03-24 19:16:42 +0000

    Automatic registration of Models in admin.py

--- noted/notes/admin.py
+++ noted/notes/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
+from django.db.models import get_models, get_app
 
-# Register your models here.
-from models import Note, Tag, Folder
-
-admin.site.register(Note)
-admin.site.register(Tag)
-admin.site.register(Folder)
+# Smarter way to register all models for an app
+for model in get_models(get_app('notes')):
+    # If you need to customise or
+    # have duplicate models exclude them
+    admin.site.register(model)
